0fdd3baad34fbaf5bb58b0b37c35ceebbe891699
Beau <beaudesrosiers@gmail.com>
2025-04-25 22:44:44 -0400

    Started Adding new getPublicVenueEvents method

--- src/edu/westfieldstate/eticketmanager/controller/GeneralScreenController.java
+++ src/edu/westfieldstate/eticketmanager/controller/GeneralScreenController.java
@@ -3,14 +3,16 @@ package edu.westfieldstate.eticketmanager.controller;
 import edu.westfieldstate.eticketmanager.core.SceneID;
 import edu.westfieldstate.eticketmanager.core.SceneManager;
 import edu.westfieldstate.eticketmanager.model.Event;
+import edu.westfieldstate.eticketmanager.model.Venue;
+import edu.westfieldstate.eticketmanager.util.JDBC;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableRow;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
+import java.sql.*;
 import java.time.LocalDate;
 
 public class GeneralScreenController
@@ -23,16 +25,28 @@ public class GeneralScreenController
     private TableColumn<Event, String> descriptionColumn;
     @FXML
     private TableColumn<Event, LocalDate> dateColumn;
+    @FXML
+    private Label noEventsLabel;
+
+    @FXML
+    private ComboBox<Venue> venueSelector;
+
+    private ObservableList<Event> tableEvents;
 
     @FXML
     public void initialize()
     {
+        tableEvents = FXCollections.observableArrayList();
+
         nameColumn.setCellValueFactory(new PropertyValueFactory<>("eventName"));
         descriptionColumn.setCellValueFactory(new PropertyValueFactory<>("eventDescription"));
         dateColumn.setCellValueFactory(new PropertyValueFactory<>("eventDate"));
 
-       // ObservableList<Event> events = Event.getAllEvents();
-       // table.setItems(events);
+        noEventsLabel = new Label("No Venue Selected");
+        table.setPlaceholder(noEventsLabel);
+
+       // tableEvents = Event.getAllEvents();
+       // table.setItems(tableEvents);
 
         // Makes table rows (events) clickable. Clicking currently just prints event name
         table.setRowFactory(tv ->
@@ -53,5 +67,33 @@ public class GeneralScreenController
 
     public void backToLogin(ActionEvent e){
         SceneManager.switchTo(SceneID.LOGIN_SCREEN);
+    }
+
+    public ObservableList<Event> getPublicVenueEvents(Venue venue)
+    {
+        String query = "SELECT event_name, event_description, event_date " +
+                       "FROM event_venue " +
+                       "WHERE is_active = 1 && venue_name = ? " +
+                       "ORDER BY eventDate";
+
+        try (Connection connection = JDBC.getConnection())
+        {
+            if(connection != null)
+            {
+                PreparedStatement ps = connection.prepareStatement(query);
+                ps.setString(1, venue.getVenueName());
+                ResultSet rs = ps.executeQuery();
+                tableEvents.clear();
+
+                while(rs.next())
+                {
+                    String eventName = rs.getString("event_name");
+                    String eventDescription = rs.getString("event_description");
+                    LocalDate date = LocalDate.parse(rs.getString("event_date"));
+                }
+            }
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
